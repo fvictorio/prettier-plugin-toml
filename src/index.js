673a1cdad84ef746e9252f4515e17b62a970b489
fvictorio <victorio.franco@gmail.com>
2019-01-22 16:02:36 -0300

    Finished

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const parser = require('toml/lib/parser')
+const {
+  doc: {
+    builders: { concat, hardline, group, indent, line, join, softline }
+  }
+} = require('prettier')
 
 const languages = [
   {
@@ -16,7 +21,37 @@ const parsers = {
 }
 
 function printToml(path, options, print) {
-  return ''
+  const node = path.getValue()
+
+  if (Array.isArray(node)) {
+    return concat(path.map(print))
+  }
+
+  switch (node.type) {
+    case 'Assign':
+      return concat([node.key, ' = ', path.call(print, 'value'), hardline])
+    case 'String':
+      return `"${node.value}"`
+    case 'Boolean':
+      return node.value.toString()
+    case 'Integer':
+      return node.value.toString()
+    case 'Date':
+      return node.value.toISOString()
+    case 'ObjectPath':
+      return concat(['[', node.value.join('.'), ']', hardline])
+    case 'Array':
+      return group(
+        concat([
+          '[',
+          indent(concat([softline, join(concat([',', line]), path.map(print, 'value'))])),
+          softline,
+          ']'
+        ])
+      )
+    default:
+      return ''
+  }
 }
 
 const printers = {
